38feb11abaff0adaa4cdcc21d793926a83168151
lijiee <1028188271@qq.com>
2019-03-08 16:52:45 +0800

    RN集成

--- ExchangeToken/ViewController.swift
+++ ExchangeToken/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  ExchangeToken
 //
-//  Created by 李杰 on 2019/2/28.
+//  Created by 李杰 on 2019/3/1.
 //  Copyright © 2019 李杰. All rights reserved.
 //
 
@@ -12,9 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
